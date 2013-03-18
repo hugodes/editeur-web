05d661ddf9232068585ea2504aa417aef2fe32d0
hamid3 <h.sakine3@gmail.com>
2013-03-18 15:08:08 +0100

    Update noeudtexte.h

---
+++ noeudtexte.h
@@ -0,0 +1,14 @@
+#ifndef domtext_h  
+#define domtext_h
+using namespace std;
+#include "DOMNode.h" 
+
+class DOMText : public DOMNode {
+
+private String text ; 
+
+public DOMText ();
+};
+
+
+#endif 
