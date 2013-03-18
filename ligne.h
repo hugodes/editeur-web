079b68af8f0f15b252e05ad9e324bacfb4156e64
Amazigh <amazigh.babbag@gmail.com>
2013-03-18 15:12:31 +0100

    Update ligne.h

---
+++ ligne.h
@@ -0,0 +1,16 @@
+#ifndef LIGNE
+#define LIGNE
+using namespace std;
+#include<vector>
+#include"buffer.h"
+
+class Ligne : public buffer 
+{
+private:
+  vector<int> identUtil;
+public:
+	Ligne();
+
+};
+
+#endif
