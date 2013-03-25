7f4dd5a83b2a48dac6718efeeaaa87aaa36f01bb
HAMID <hamid@ubuntu.ubuntu-domain>
2013-03-25 13:12:08 +0100

    la classe  noeudtexte.h

---
+++ noeudtexte.h
@@ -0,0 +1,17 @@
+#ifndef domtext_h
+#define domtext_h
+using namespace std;
+#include "DOMNode.h"
+#include <vector>
+class DOMText{
+
+private :
+    vector<string> noeudtext;
+
+public :
+         DOMText ();
+         ~DOMText() ;
+};
+
+
+#endif
