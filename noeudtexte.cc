91a2ed69bfebc41130ad118cd5e4e7440a7206fe
HAMID <hamid@ubuntu.ubuntu-domain>
2013-04-12 14:42:32 +0200

    ajout noeudtext

---
+++ noeudtexte.cc
@@ -0,0 +1,19 @@
+#ifndef domtext_h
+#define domtext_h
+using namespace std;
+#include "DOMNode.h"
+
+
+DOMText :: DOMText() {
+
+}
+
+DOMText ::~DOMText() {
+
+}
+
+DOMText::DOMText(string s){
+	text=s;
+}
+
+
