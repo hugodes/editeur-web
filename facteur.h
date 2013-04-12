86fa4094eccbb41a55fbe616fd37091fb1a83f6f
nicolas-emeri <niko314@hotmail.fr>
2013-04-12 18:56:09 +0300

    Update facteur.h
    Commentaires doxygen
    Surchage operateur <<

--- facteur.h
+++ facteur.h
@@ -1,6 +1,15 @@
+/**
+* @file facteur.h
+* @author Nicolas EMERI & Bryan LIBOUREL
+* @details surcharge de l'operateur <<
+*/
+
 #ifndef DEF_FACTEUR
 #define DEF_FACTEUR
 
+/**
+* @class facteur facteur.h "./facteur.h"
+*/
 class facteur {
 
 	private:
@@ -9,13 +18,15 @@ class facteur {
 		char* texteFormate;
 
 	public:
-		facteur(char* text);
-		~facteur();
-		void setTexte(char* text);
-		char* getTexte();
-		char* getTexteFormate();
-		void setCouleur (char* color);
-		void formate();
+		facteur(char* text); /** @fn Constructeur de facteur */
+		~facteur(); /** @fn Destructeur de facteur */
+		void setTexte(char* text); /** @fn Definir le texte  */
+		char* getTexte(); /** @fn Retourne le texte du facteur */
+		char* getTexteFormate(); /** @fn Retourne le texte du facteur formaté avec la couleur */
+		void setCouleur (char* color); /** @fn Definir la couleur */
+		void formate(); /** @fn Formate puis stock le texte */
 };
+
+ostream& operator<<(ostream &flux, Duree const& facteur); /** @fn Surcharge l'operateur << */
 
 #endif
