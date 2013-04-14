4f6cfe9dc35fc12413d88837e435ff8912165976
Bryan L <libourelbryan@gmail.com>
2013-04-14 18:58:15 +0200

    Fichier projet, main et interface graphique
    J'ai donc créé le fichier du projet principal qui est gérable par
    beaucoup d'IDE (normalement).
    L'interface graphique ne prends pas encore tout, mais beaucoup trop
    d'erreurs dans les autres classes pour pouvoir obtenir quelque chose !

--- leedit.cpp
+++ leedit.cpp
@@ -1,3 +1,14 @@
+/**
+ * \file leedit.cpp
+ * \brief classe de l'interface graphique
+ * \author Bryan Libourel
+ * \version 0.2
+ * \date 12 avril 2013
+ *
+ * Interface graphique de l'éditeur de texte
+ *
+ */
+
 #include "leedit.h"
 #include "ui_leedit.h"
 
@@ -9,8 +20,12 @@ LeEdit::LeEdit(QWidget *parent) :
 
     QObject::connect(ui->actionDom, SIGNAL(triggered(bool)), this, SLOT(affichageDOM(bool)));
     QObject::connect(ui->actionHierarchie, SIGNAL(triggered(bool)), this, SLOT(affichageHIE(bool)));
-    QObject::connect(ui->actionApercu, SIGNAL(triggered()), this, SLOT(affichageAPE()));
     QObject::connect(ui->onglets, SIGNAL(currentChanged(int)), this, SLOT(affichageWID(int)));
+    QObject::connect(ui->actionApercu, SIGNAL(triggered()), this, SLOT(affichageAPE()));
+    QObject::connect(ui->actionOuvrir, SIGNAL(triggered()), this, SLOT(boiteOuvrir()));
+    QObject::connect(ui->actionOuvrir, SIGNAL(triggered()), this, SLOT(modificationHIE()));
+    QObject::connect(ui->actionNouveau, SIGNAL(triggered()), this, SLOT(nouveauFichier()));
+
 
 }
 
@@ -43,4 +58,28 @@ void LeEdit::affichageWID(int i) {
     if(i == 1) {
         ui->apercu->setHtml(ui->code->toPlainText());
     }
+}
+
+void LeEdit::boiteOuvrir() {
+    QString fichier = QFileDialog::getOpenFileName(this, "Ouvrir un fichier", "C:\\", "Fichier web (*.css *.js *.html *.php),*.css;*.js;*.html;*.php");
+    QDir chemin = QFileInfo(fichier).absoluteDir();
+    cheminFichier = chemin.absolutePath();
+    //QMessageBox::information(this, "Fichier", "Vous avez sélectionné :"+cheminFichier+"\n");
+    buff = new Buffer(fichier.toLatin1().data());
+    //QMessageBox::information(this, "Fichier", buff);
+    //ui->code->setText(buf.affiche(););
+}
+
+void LeEdit::nouveauFichier() {
+    buff = new Buffer();
+}
+
+void LeEdit::modificationHIE() {
+        QFileSystemModel *dirModele= new QFileSystemModel;
+        dirModele->setRootPath(cheminFichier);
+
+        ui->hierarchie->setModel(dirModele);
+        ui->hierarchie->expandAll();
+        ui->hierarchie->setColumnWidth(0,ui->hierarchie->width());
+
 }
