afd60305803e208d6c1a2264d0a82e24509d182a
Hugo Des <hugo.deslongchamps@gmail.com>
2013-04-14 16:21:16 +0200

    Commencement des tests

--- test.cc
+++ test.cc
@@ -1,10 +1,19 @@
 //createur Hugo des Longchamps
 #include <iostream>
+#include <cstring>
 using namespace std;
 
-#include "buffer.h"
+//#include "buffer.h"
+#include "facteur.h"
 
 int main(){
     //ecrire vos tests ici
+    string str("UnMmot");
+    char *cstr = new char[str.length()+1];
+    strcpy(cstr, str.c_str());
+    cout<<"Je suis dans le main"<<endl;
+    Facteur f(cstr);
+    cout<<"Voici ce qu'affiche f"<<endl;
+    cout<<f<<endl;
     return 0;
 }
