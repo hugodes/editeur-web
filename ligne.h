7e9a49eb84c1553199fb539fd4d9c03008571372
Bryan L. <libourelbryan@gmail.com>
2013-05-21 17:41:59 +0200

    Interface graphique finie ?
    L'ensemble des fichiers nécessaires à l'interface graphique.
    Interface presque finie, manque plus que le DOM et le Buffer

---
+++ ligne.h
@@ -0,0 +1,38 @@
+/**
+*@author Amazigh Haddadou 
+*/
+#ifndef DEF_LIGNE
+#define DEF_LIGNE
+
+#include <vector>
+#include <string>
+#include <cstring>
+#include <iterator>
+#include <iostream>
+using namespace std;
+#include "facteur.h"
+/**
+* @classe Facteur facteur.h
+*/
+class Ligne
+{
+
+	private:
+		int indentUtil;
+		vector<Facteur> ligne;
+
+	public:
+		Ligne();/** @fn Constructeur par defaut de ligne */
+		Ligne(vector<Facteur>); /** @fn Constructeur de ligne */
+		Ligne(char*&); /** @fn Constructeur de ligne */
+		Ligne(const Ligne&);/** @fn Constructeur par copie */
+		~Ligne(); /** @fn Destructeur de ligne */
+		char* toCString(); /** @fn Retourne une ligne */
+		string toString(); /** @fn Retourne une ligne */
+		void affiche(ostream &)const; /** @fn Retourne le text d'une ligne */
+		//Ligne& operator=(const Ligne&);/** @fn surcharge de l'opérateur = */
+};
+
+ostream& operator<<(ostream &,const Ligne &); /** Surcharge de l'operateur << */
+
+#endif
